54f01b927959ea66bdc1f2e486a735a8ed1ee1db
FiverTorr <louviss.herve@gmail.com>
2018-10-29 15:09:30 +0100

    Bug fix

--- game.js
+++ game.js
@@ -192,7 +192,7 @@ function diagonalCrossed() {
 
   if (board[2][0] === board[1][1] &&
     board[1][1] === board[0][2] &&
-    board[0][0] !== NONE) {
+    board[2][0] !== NONE) {
     return true;
   }
 
